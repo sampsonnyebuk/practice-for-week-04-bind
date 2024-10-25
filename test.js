eedcec04b94b65a34d521468ed4ebd2f7741058a
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-10-25 14:43:28 +0100

    first commit

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 // Your code here
+
+import Employee from "./employee.js";
+
+const employee = new Employee("John Wick", "Dog Lover");
+
+setTimeout(employee.sayName.bind(employee), 2000);
+
+setTimeout(employee.sayOccupation.bind(employee), 3000);
